0633c0c244a3fad7d5c188c6b26832342af2a305
AndrewL <lawna12@gmail.com>
2020-02-15 19:41:05 -0500

    established express, added package.json, and path

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -1,3 +1,13 @@
-const express = require('express')
-const path = require('path')
-const fs = require("fs")
+const express = require('express');
+const path = require('path');
+const fs = require("fs");
+
+const app = express();
+var PORT = process.env.PORT || 3660;
+
+
+
+
+app.listen(PORT, () => {
+    console.log("APP listening on PORT " + PORT)
+})
